27159d8ff00256ec635e8dc37f2cd48ac6775d70
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-06 17:26:24 +0530

    changing texts

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function App() {
       <nav>Toggle Goes Here</nav>
       <main>
         <h1>"Light Mode"</h1>
-        <h2>Toggle the switch to see some magic happen!</h2>
+        <h2>Toggle for dark Mode</h2>
       </main>
     </div>
   );
