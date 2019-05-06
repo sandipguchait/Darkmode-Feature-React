28a45f7c89407a6e25503e7fcdf973c4e1c3402a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-06 17:38:13 +0530

    implementing the toggle feature

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const handletoggle = () => {
+    //using previous state to update the toggle
+    setDarkMode(prevMode => !prevMode);
+  };
+
   return (
-    <div className="light-mode">
-      <nav>Toggle Goes Here</nav>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <nav>
+        <div className="toggle-container">
+          <button onClick={handletoggle}>Toggle Mode</button>
+        </div>
+      </nav>
       <main>
-        <h1>"Light Mode"</h1>
+        <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
         <h2>Toggle for dark Mode</h2>
       </main>
     </div>
