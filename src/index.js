68f4bf08cac75aae6939abf4d2d962dbeb28dc75
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-06 17:46:50 +0530

    completing the darkmode feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
-  const [darkMode, setDarkMode] = React.useState(false);
+  const [darkMode, setDarkMode] = React.useState(getinitialMode());
+
+  React.useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getinitialMode() {
+    const savedMode = JSON.parse(localStorage.getItem("dark"));
+    return savedMode || false;
+  }
 
   const handletoggle = () => {
     //using previous state to update the toggle
